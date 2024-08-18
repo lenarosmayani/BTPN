a1733a9098c24fb8be331f57b3cb20c5a1e9451f
lenarosmayani <rosmayanilena@gmail.com>
2024-08-18 20:28:01 +0700

    modified:   web-store-fe/angular.json 	modified:   web-store-fe/src/app/app.component.html 	modified:   web-store-fe/src/app/app.routes.ts 	new file:   web-store-fe/src/app/customers/customers.component.css 	new file:   web-store-fe/src/app/customers/customers.component.html 	new file:   web-store-fe/src/app/customers/customers.component.spec.ts 	new file:   web-store-fe/src/app/customers/customers.component.ts 	new file:   web-store-fe/src/app/items/items.component.css 	new file:   web-store-fe/src/app/items/items.component.html 	new file:   web-store-fe/src/app/items/items.component.spec.ts 	new file:   web-store-fe/src/app/items/items.component.ts 	new file:   web-store-fe/src/app/orders/orders.component.css 	new file:   web-store-fe/src/app/orders/orders.component.html 	new file:   web-store-fe/src/app/orders/orders.component.spec.ts 	new file:   web-store-fe/src/app/orders/orders.component.ts

--- web-store-fe/src/app/app.routes.ts
+++ web-store-fe/src/app/app.routes.ts
@@ -1,3 +1,20 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ItemsComponent } from './items/items.component';
+import { CustomersComponent } from './customers/customers.component';
+import { OrdersComponent } from './orders/orders.component';
 
-export const routes: Routes = [];
+// Definisikan routes
+const routes: Routes = [
+  { path: 'customers', component: CustomersComponent },
+  { path: 'items', component: ItemsComponent },
+  { path: 'orders', component: OrdersComponent },
+  { path: '', redirectTo: '/customers', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+export { routes };
